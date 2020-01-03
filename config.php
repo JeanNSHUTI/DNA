546ef5b5e5ec679c8fbbe10f9c87fd6b11bc82db
jeanNSHUTI <jean.rene.nshuti@gmail.com>
2020-01-03 17:55:42 +0100

    Changed php code for login page

--- config.php
+++ config.php
@@ -1,7 +1,14 @@
 <?php
-   define('DB_SERVER', 'localhost:3036');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'pass99word');
-   define('DB_DATABASE', 'db_dna');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', 'pass99word');
+    define('DB_DATABASE', 'db_dna');
+
+    /* Attempt to connect to MySQL database */
+    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+    // Check connection
+    if($link === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
 ?>

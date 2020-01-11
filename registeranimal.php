bbc3045db7c60d9bea6e555e9c53584e3c78425b
jeanNSHUTI <jean.rene.nshuti@gmail.com>
2020-01-12 00:12:19 +0100

    added session check to registration pages

--- registeranimal.php
+++ registeranimal.php
@@ -1,22 +1,85 @@
 <?php
-   //include('session.php');
-   include("config.php");
-   //session_start();
-
-   if(isset($_POST['submitanimal'])){
-   $new_name = mysqli_real_escape_string($link,$_POST['inputName']);
-   $new_animalID = mysqli_real_escape_string($link,$_POST['inputAID']); 
-   $new_weight = mysqli_real_escape_string($link,$_POST['inputWeight']); 
-   $new_yearofbirth = mysqli_real_escape_string($link,$_POST['inputYOB']); 
-   }
-
-   $sql = "INSERT INTO animal (animal_id, createdBy, name, dateOfbirth, actualweight) VALUES ('$new_animalID', 'sU44222', '$new_name', '$new_yearofbirth', '$new_weight')";
-
-    if ($link->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $link->error;
-    }
-
-    $link->close();
+   include('session.php');
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SB Admin - Register new Animal</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-dark">
+
+  <div class="container">
+    <div class="card card-register mx-auto mt-5">
+      <div class="card-header">Register a new Animal</div>
+      <div class="card-body">
+        <form action="registeranimal.php" method="post">
+          <div class="form-group">
+            <div class="form-row">
+              <div class="col-md-6">
+                <div class="form-label-group">
+                  <input type="text" id="inputName" name="inputName" class="form-control" placeholder="Name" required="required" autofocus="autofocus">
+                  <label for="inputName">Animal Name</label>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-label-group">
+                  <input type="text" id="inputAID" name="inputAID" class="form-control" placeholder="Animal ID" required="required">
+                  <label for="inputAID">Animal ID</label>
+                </div>
+              </div>
+            </div>
+          </div>           
+          <div class="form-group">
+            <div class="form-row">
+              <div class="col-md-6">
+                <div class="form-label-group">
+                  <input type="text" id="inputWeight" name="inputWeight" class="form-control" placeholder="Weight" required="required">
+                  <label for="inputWeight">Actual Weight (Kg)</label>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-label-group">
+                  <input type="text" id="inputYOB" name="inputYOB" class="form-control" placeholder="Year of Birth" required="required">
+                  <label for="inputYOB">Year Of Birth</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!--<a type = "submit" class="btn btn-primary btn-block" href="login.html">Register</a>-->
+          <input name="submitanimal" type = "submit" class="btn btn-primary btn-block" value = " Register Animal "/><br />
+        </form>
+        <div class="text-center">
+          <a class="d-block small mt-3" href="javascript:history.go(-1)">Cancel</a>
+          <!-- <a class="d-block small" href="forgot-password.html">Forgot Password?</a> -->
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+</body>
+
+</html>
---
+++ registerdb.php
@@ -0,0 +1,30 @@
+<?php
+   include('session.php');
+   include("config.php");
+   //session_start();
+
+   if(isset($_POST['submituser'])){
+       $new_firstname = mysqli_real_escape_string($link,$_POST['firstName']);
+       $new_lastname = mysqli_real_escape_string($link,$_POST['lastName']); 
+       $new_email = mysqli_real_escape_string($link,$_POST['inputEmail']); 
+       $new_username = mysqli_real_escape_string($link,$_POST['inputUsername']); 
+       $new_password = mysqli_real_escape_string($link,$_POST['inputPassword']);
+       $new_confirmpassword = mysqli_real_escape_string($link,$_POST['confirmPassword']);
+   }
+
+   /*echo $new_firstname;
+   echo $new_lastname;
+   echo $new_email;
+   echo $new_username;*/
+
+   $sql = "INSERT INTO users (username, firstname, surname, passcode, super_user, email) VALUES ('$new_username', '$new_firstname', '$new_lastname', '$new_password', FALSE, '$new_email')";
+
+    if ($link->query($sql) === TRUE) {
+        echo "New record created successfully";
+        // register change in notifications table here !!
+    } else {
+        echo "Error: " . $sql . "<br>" . $link->error;
+    }
+
+   $link->close();
+?>

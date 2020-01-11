84958945dddebd606c627e8df0aa0427f1cc385a
jeanNSHUTI <jean.rene.nshuti@gmail.com>
2020-01-12 00:34:28 +0100

    added session check to registration pages

--- session.php
+++ session.php
@@ -1,14 +1,6 @@
 <?php
-   include('config.php');
-   session_start();
-   
-   $user_check = $_SESSION['User_email'];
-   
-   $ses_sql = mysqli_query($link,"SELECT email FROM users WHERE email = '$user_check' ");
-   
-   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-   
-   $login_session = $row['email'];
+   include("config.php");
+   session_start();   
    
    if(!isset($_SESSION['User_email'])){
       header("location:index.php");

9baa6cebf2671bbad91da1472687acfe7a8b36ad
jeanNSHUTI <jean.rene.nshuti@gmail.com>
2020-01-15 02:05:16 +0100

    implemented modification page with jquery

--- animalupdate.php
+++ animalupdate.php
@@ -5,13 +5,16 @@
     $field = $_POST['field'];
     $value = $_POST['value'];
     $editid = $_POST['id'];
+    $field = mysqli_real_escape_string($link,$_POST['field']);
+    $value = mysqli_real_escape_string($link,$_POST['value']);
+    $editid = mysqli_real_escape_string($link,$_POST['editid']);
 
     $sql = "UPDATE animal SET ".$field."='".$value."' WHERE id=".$editid;
-    //mysqli_query($link,$sql);
+    mysqli_query($link,$sql);
 
-    //echo 1;
+    echo 1;
 
-    if ($link->query($sql) === TRUE) {
+/*    if ($link->query($sql) === TRUE) {
         //echo "New record created successfully";
         echo "<script type='text/javascript'>alert(' . $editid . has been modified');
         window.location='dashboard.php';
@@ -24,5 +27,5 @@
     }
 
    $link->close();
-?>
+?>*/
 

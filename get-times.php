73c875e8ec4606add56b1732c6a99a9cdf617fd9
jeanNSHUTI <jean.rene.nshuti@gmail.com>
2020-01-21 03:37:27 +0100

    Implemmented embedded system get data

--- get-times.php
+++ get-times.php
@@ -5,7 +5,7 @@
     $api_key_value = "tPmAT5Ab3j7F9";
 
     $apikey    =   $_GET['api_key'];
-    $apikey  = mysqli_real_escape_string($link,$_GET['api_key']);
+    //$apikey  = mysqli_real_escape_string($link,$_GET['api_key']);
     
     if($apikey == $api_key_value){
         $sql        =   "SELECT downtime_startTime, downtime_endTime FROM system_schedule ORDER BY id DESC LIMIT 1";
@@ -15,13 +15,16 @@
 
         while ($row = $result->fetch_assoc())
         {
-            echo $row['downtime_startTime' . + "_"];
+            echo $row['downtime_startTime'];
+            echo "_";
             echo $row['downtime_endTime'];
         }
 
         } else {
             echo "Error:" . $sql . "<br>" . $link->error;
         }                
+    } else{
+        echo "Wrong api";
     }
     
     $link->close();
